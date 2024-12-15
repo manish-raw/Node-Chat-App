0edcdcd02a6ce4bd8255ee89d54ee7195d96e82e
manish-raw <manishyadav.y31@gmail.com>
2024-12-15 15:43:07 +0530

    function implemented

--- client.js
+++ client.js
@@ -6,8 +6,46 @@ const rl = readline.createInterface({
     output: process.stdout,
 });
 
+const clearLine = (dir) => {
+    return new Promise((resolve, reject) =>{
+        process.stdout.clearLine(dir, ()=>{
+            resolve();
+        });
+    });
+};
+
+const moveCursor = (dx, dy) =>{
+    return new Promise((resolve, reject)=>{
+        process.stdout.moveCursor(dx, dy, ()=>{
+            resolve();
+        });
+    });
+};
+
 const socket = net.createConnection({host: "127.0.0.1",port: 3099}, async ()=>{
 
-    const message = await rl.question("type something > ")
-    socket.write(message);
-})
+    const ask= async ()=>{
+        const message = await rl.question("type something > ");
+        await moveCursor(0, -1);
+        await clearLine(0);
+        //below command will wait for the above line to respond back
+        socket.write(message);
+    };
+
+    ask();
+
+    socket.on("data",async (data)=>{
+        console.log();
+        await moveCursor(0, -1);
+        await clearLine(0);
+        console.log(data.toString("utf-8"));
+        ask();
+    });
+
+});
+
+
+
+socket.on("end", ()=>{
+    console.log("Connection was ended!");
+});
--- server.js
+++ server.js
@@ -2,17 +2,21 @@ import net from "node:net";
 
 const server = net.createServer();
 
+const clients = [];
+
 server.on("connection", (socket)=>{
+
     console.log("A new connection to server!");
-
-
-
-
     socket.on("data", (data)=>{
-        console.log(data.toString("utf-8"));
-    })
-});
+        //writing data in each socket
+        clients.map((s)=>{
+            s.write(data);
+        })
+    });
+    //For sending msg to every client
+    clients.push(socket);
 
+});
 
 server.listen(3099, "127.0.0.1", ()=>{
     console.log("Server is running....", server.address());
